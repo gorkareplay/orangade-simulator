a9fae027fd8dddd8ba4068592ee25afa5c6be886
gorkareplay <gorkareplay@gmail.com>
2026-06-17 01:37:50 +0200

    who's the stinky programmer now?

--- scenes/scripts/level_2.gd
+++ scenes/scripts/level_2.gd
@@ -1,5 +1,6 @@
 extends Control
 
+signal level_2_complete
 var tile_path = preload("res://scenes/tscn/tile.tscn")
 var scoring_tiles: Array = [6, 7, 11, 12, 33, 34, 36, 39, 53, 54, 55, 56, 70, 71, 72, 75, 76, 85, 86, 91, 93, 95, 96]
 
@@ -23,10 +24,6 @@ func _ready() -> void:
 		tile.position = origin_point + Vector2(i % cols, i / cols) * (tile_size + spacing)
 		button.pressed.connect(_button_pressed.bind(i, button))
 		add_child(tile)
-	
-func _process(_delta: float) -> void:
-	if score == 20:
-		emit_signal("level_2_complete")
 
 func _button_pressed(index: int, button: Button) -> void:
 	button.disabled = true
@@ -35,5 +32,7 @@ func _button_pressed(index: int, button: Button) -> void:
 	if index in scoring_tiles:
 		tile.texture = tile_tick
 		score += 1
+		if score == 20:
+			emit_signal("level_2_complete")
 	else:
 		tile.texture = tile_cross
--- scenes/scripts/root.gd
+++ scenes/scripts/root.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var title_screen = preload("res://scenes/tscn/title_screen.tscn").instantiate() as Control
 var play_button = title_screen.get_node("PlayButton") as Button
+var level_1 = preload("res://scenes/tscn/levels/level_1.tscn").instantiate()
+var line_edit = level_1.get_node("LineEdit") as LineEdit
+var level_2 = preload("res://scenes/tscn/levels/level_2.tscn").instantiate()
+#var level_3 = preload("res://scenes/tscn/levels/level_3.tscn").instantiate()
 var level_4 = preload("res://scenes/tscn/levels/level_4.tscn").instantiate()
 
 func _ready() -> void:
@@ -13,4 +17,28 @@ func _ready() -> void:
 
 func _on_play_pressed():
 	play_button.queue_free()
+	add_child(level_1)
+	
+	line_edit.level_1_complete.connect(_on_level_1_complete)
+
+func _on_level_1_complete():
+	level_1.queue_free()
+	add_child(level_2)
+	
+	level_2.level_2_complete.connect(_on_level_2_complete)
+
+func _on_level_2_complete():
+	level_2.queue_free()
+#	add_child(level_3)
+
+#	level_3.level_3_complete.connect(_on_level_3_complete)
 	add_child(level_4)
+
+func _on_level_3_complete():
+#	level_3.queue_free()
+	add_child(level_4)
+	
+	level_4.level_4_complete.connect(_on_level_4_complete)
+
+func _on_level_4_complete():
+	print("Плаке плаке")
