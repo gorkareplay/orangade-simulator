4c3dcb0394661be8f5159f98ba71104acad785ff
untugo <arseniy2010kolodin@gmail.com>
2026-06-16 12:52:59 +0200

    title screen + background
    added a title screen with an interactable play button. added the background and it's shaders from my other project. started the script in the root scene to change levels

--- scenes/scripts/root.gd
+++ scenes/scripts/root.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-var fruit_scene = preload("res://scenes/tscn/fruits.tscn")
+var title_screen = preload("res://scenes/tscn/title_screen.tscn").instantiate() as Control
+var play_button = title_screen.get_node("PlayButton") as Button
+var level_1 = preload("res://scenes/tscn/levels/level_1.tscn").instantiate()
 
-
-func _on_timer_timeout() -> void:
-	var fruit = fruit_scene.instantiate() as RigidBody2D
-	var positions = $Positions.get_children().pick_random() as Node2D
+func _ready() -> void:
 	
-	fruit.position = positions.position
-	add_child(fruit)
+	title_screen.position = Vector2.ZERO
+	add_child(title_screen)
+	
+	play_button.pressed.connect(_on_play_pressed)
+
+func _on_play_pressed():
+	play_button.queue_free()
+	add_child(level_1)
