684c2d5114b7bc213f04a3c0f8c65cf234823b03
untugo <arseniy2010kolodin@gmail.com>
2026-06-18 11:28:38 +0200

    finished aptyp (pre-polish)

--- scenes/scripts/root.gd
+++ scenes/scripts/root.gd
@@ -4,7 +4,7 @@ var title_screen = preload("res://scenes/tscn/title_screen.tscn").instantiate() 
 var play_button = title_screen.get_node("PlayButton") as Button
 var level_1 = preload("res://scenes/tscn/levels/level_1.tscn").instantiate()
 var line_edit = level_1.get_node("LineEdit") as LineEdit
-var level_2 = preload("res://scenes/tscn/levels/level_2.tscn").instantiate()
+var level_2 = preload("res://scenes/tscn/levels/level_2.tscn").instantiate() as Control
 #var level_3 = preload("res://scenes/tscn/levels/level_3.tscn").instantiate()
 var level_4 = preload("res://scenes/tscn/levels/level_4.tscn").instantiate()
 
@@ -16,40 +16,75 @@ func _ready() -> void:
 	add_child(title_screen)
 	
 	play_button.pressed.connect(_on_play_pressed)
-	aptyp("hello my name is black aptyp and \nim going to freaking stuff you")
 	
-func aptyp(line: String, delay: float = 0.1) -> void:
+func aptyp(line: String, delay: float = 0.1, time: float = 1.0) -> void:
 	label.visible_characters = 0
 	label.text = line
 	
 	for i in range(len(line)):
 		label.visible_characters = i + 1
 		await get_tree().create_timer(delay).timeout
-
+	await get_tree().create_timer(time).timeout
 func _on_play_pressed():
 	play_button.queue_free()
+	await aptyp("hey")
+	await aptyp("what the hell are you doing in my game?", 0.05, 2.0)
+	await aptyp("lemonade?...", 0.2, 2.0)
+	await aptyp("okay", 0.05, 0.5)
+	await aptyp("in order to get your lemonade, you need to solve 4 puzzles", 0.05, 2.0)
+	await aptyp("in this puzzle you need to guess the code", 0.05, 2.0)
+	await aptyp("the code is 1234, but since you're so EMPTY headed there is NOTHING you can do", 0.1, 1.0)
 	add_child(level_1)
 	
 	line_edit.level_1_complete.connect(_on_level_1_complete)
 
 func _on_level_1_complete():
 	level_1.queue_free()
+	await aptyp("how the hell did you do that", 0.05, 1.0)
+	await aptyp("doesn't matter, here is one of the ingredients", 0.05, 2.0)
+	# give nig ingredient or something
+	await aptyp("okay next puzzle", 0.05, 0.5)
+	await aptyp("this is a game of battleship, i hope you know the rules", 0.05, 3.0)
+	await aptyp("you are trying to guess the positions of my ships and wise-versa", 0.05, 3.0)
+	await aptyp("you can just click to shoot my ships", 0.05, 3.0)
+	level_2.position = Vector2(415, 70)
 	add_child(level_2)
 	
 	level_2.level_2_complete.connect(_on_level_2_complete)
-
+	await aptyp("you NEED to follow the TURN ORDER", 0.05, 2.0)
+	await aptyp("okay my turn now...", 0.1, 4.0)
+	await aptyp("hey what are you doing", 0.05)
+	
 func _on_level_2_complete():
 	level_2.queue_free()
+	await aptyp("dude", 0.01, 0.1)
+	await aptyp("okay you certanly cheated there", 0.05, 1.0)
+	await aptyp("fine take the ingredient i don't care", 0.05, 2.0)
+	# give something
+	await aptyp("let's go next puzzle", 0.05, 1.0)
+	await aptyp("so this is an impossible labyrinth i made that you need to complete", 0.05, 3.0)
 #	add_child(level_3)
-
 #	level_3.level_3_complete.connect(_on_level_3_complete)
-	add_child(level_4)
+	await aptyp("sadly i was not able to finish it so just IGNORE the eraser shavings", 0.05)
 
 func _on_level_3_complete():
 #	level_3.queue_free()
+	await aptyp("BRUH HOW ARE YOU DOING THIS STOP IT ALREADY!!!", 0.01)
+	await aptyp("good job, i guess", 0.1, 2.0)
+	await aptyp("there you go", 0.075, 1.0)
+	# give penis surprise
+	await aptyp("and so, the final puzzle awaits", 0.05, 2.0)
+	await aptyp("are you ready?", 0.2, 1.0)
 	add_child(level_4)
 	
 	level_4.level_4_complete.connect(_on_level_4_complete)
-
+	await aptyp("in this puzzle, you need to sort the fruits by color", 0.05, 2.0)
+	await aptyp("sort them by color, and ignore the NUMBER OF LEAVES", 0.05)
+	
 func _on_level_4_complete():
-	print("Плаке плаке")
+	level_4.queue_free()
+	await aptyp("NOOOOOOOOOOOOOOOOOO", 0.01)
+	await aptyp("")
+	# give final ingredient
+	# play giga game ending animation
+	# game finished yay
