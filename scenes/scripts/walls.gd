12104817664e056cb6d5dad9961797a0e569f71c
gorkareplay <gorkareplay@gmail.com>
2026-06-19 13:27:19 +0200

    add buttons on level 3 for mobile

--- scenes/scripts/walls.gd
+++ scenes/scripts/walls.gd
@@ -1,7 +1,22 @@
 extends Node2D
 
+var left: bool = false
+var right: bool = false
+
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") or left:
 		self.rotate(-0.5 * delta)
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") or right:
 		self.rotate(0.5 * delta)
+
+func _on_left_button_down() -> void:
+	left = true
+
+func _on_left_button_up() -> void:
+	left = false
+
+func _on_right_button_down() -> void:
+	right = true
+
+func _on_right_button_up() -> void:
+	right = false
